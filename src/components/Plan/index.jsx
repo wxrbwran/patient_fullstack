4ca0af9be3cdbcc0b836718e3de24f9b7d0cdcb5
xiaoran <wxrbw@qq.com>
2017-08-28 18:14:24 +0800

    feat($plan): 添加plan页面

--- src/components/Plan/index.jsx
+++ src/components/Plan/index.jsx
@@ -2,7 +2,11 @@
  * Created by wuxiaoran on 2017/8/10.
  */
 import React, { Component } from 'react';
+import { NavBar, Tabs } from 'antd-mobile';
 // import { PropTypes } from 'prop-types';
+import style from './index.scss';
+
+const TabPane = Tabs.TabPane;
 
 class Plan extends Component {
   static propTypes = {
@@ -14,7 +18,44 @@ class Plan extends Component {
   }
   render() {
     return (
-      <div>PlanPlanPlanPlanPlan</div>
+      <div className={style.plan}>
+        <NavBar
+          mode="light"
+          className={style.nav}
+          leftContent={
+            (<img
+              className={style.plan__img}
+              src={require('./img/history@3x.png')}
+              alt="用药历史"
+            />)}
+          rightContent={
+            (<img
+              className={style.plan__img}
+              src={require('./img/common_add@3x.png')}
+              alt="新增用药"
+            />)}
+        >
+          服药
+        </NavBar>
+        <div className={style.plan__content}>
+          <Tabs defaultActiveKey="1" animated >
+            <TabPane tab="服药单" key="1">
+              <div
+                className={style.plan__lists}
+              >
+                Content of First Tab
+              </div>
+            </TabPane>
+            <TabPane tab="服药计划" key="2">
+              <div
+                className={style.plan__plans}
+              >
+                Content of Second Tab
+              </div>
+            </TabPane>
+          </Tabs>
+        </div>
+      </div>
     );
   }
 }
