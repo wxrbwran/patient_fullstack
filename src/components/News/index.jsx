b0ecb19baf97eade063b099ae5f8ea5c768ad5eb
wxrbwran <wxrbw@qq.com>
2017-10-15 14:38:37 +0800

    feat($news): 添加news页面

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -2,51 +2,61 @@
  * Created by wuxiaoran on 2017/8/10.
  */
 import React, { Component } from 'react';
+import { NavBar } from 'antd-mobile';
 // import { PropTypes } from 'prop-types';
-import { WhiteSpace, WingBlank,
-  Button, Modal } from 'antd-mobile';
+import style from './index.scss';
+import doctor from './img/news_icon_doctor@2x.png';
+import assistant from './img/news_icon_nurse@2x.png';
 
-class News extends Component {
+
+class Plan extends Component {
   static propTypes = {
   };
   constructor(props) {
     super(props);
     this.state = {
-      isShowModal: false,
     };
-  }
-  toggleModal = () => {
-    this.setState({
-      isShowModal: !this.state.isShowModal,
-    });
   }
   render() {
     return (
-      <div>
-        <WhiteSpace />
-        <WingBlank>
-          <Button
-            onClick={this.toggleModal}
-          >Modal 对话框 (自动检测平台)</Button>
-        </WingBlank>
-        <WhiteSpace />
-        <Modal
-          title="这是 title"
-          transparent
-          maskClosable
-          visible={this.state.isShowModal}
-          onClose={this.toggleModal}
-          footer={[{
-            text: '确定',
-            onPress: () => { this.toggleModal(); },
-          }]}
-        >
-          这是内容...<br />
-          这是内容...<br />
-        </Modal>
+      <div className={style.news}>
+        <NavBar
+          mode="light"
+          className={style.nav}
+          leftContent={null}
+        >消息
+        </NavBar>
+        <div className={style.content}>
+          <div className={style.doctor}>
+            <img src={doctor} alt="doctor" />
+            <div>
+              <h3>
+                医生提醒
+              </h3>
+              <p>
+                您最近做过哪些检查？
+              </p>
+              <span>
+                17:27
+              </span>
+            </div>
+          </div>
+          <div className={style.assistant}>
+            <img src={assistant} alt="assistant" />
+            <div>
+              <h3>医学助理</h3>
+              <p>
+                您了解自己的耐药性吗？
+              </p>
+              <span>
+                17:27
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default News;
+export default Plan;
