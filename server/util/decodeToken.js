3b736ef41d6b1fbaa602b9c26d016ba5d17c7fde
wxrbwran <wxrbw@qq.com>
2017-10-15 22:31:13 +0800

    feat($health): 添加health页面

--- server/util/decodeToken.js
+++ server/util/decodeToken.js
@@ -1,13 +1,11 @@
 /**
  * Created by wuxiaoran on 2017/8/22.
  */
-const jsonwebtoken  = require('jsonwebtoken');
+const jsonwebtoken = require('jsonwebtoken');
 const config = require('../config');
 
 export function decodeToken(token) {
-  console.log('received:', token);
-  const decode = jsonwebtoken.verify(token.split(' ')[1],
+  const decode = jsonwebtoken.verify(token,
     config.token.secret);
-  console.log('decoded:', decode);
   return decode;
 }
